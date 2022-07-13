146179aa6da982505cf1fd3530f809482e322432
RodrigoMassucci <rodrigo.massucci@outlook.com>
2022-07-13 17:03:48 -0300

    API test

---
+++ backend/src/main/java/com/massucci/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.massucci.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.massucci.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
